b57c22b1d22be5a67e0d3ec1511830974828739d
alabliuk <andrelabliuk@gmail.com>
2021-07-10 19:39:40 -0300

    add if de validacao no menu

--- src/0003_monitoramento_sites/monitoramento.go
+++ src/0003_monitoramento_sites/monitoramento.go
@@ -6,11 +6,20 @@ import (
 
 func main() {
 	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir Logs")
+	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair")
 
 	var comando int
 	fmt.Scan(&comando)
+	print("--> ", comando, " || ")
 
-	println("-->", comando)
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Recuperando logs...")
+	} else if comando == 0 {
+		fmt.Println("Aplicação Encerrada")
+	} else {
+		fmt.Println("Entrada Inválida!!!")
+	}
 }
